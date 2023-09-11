a07947ffddd2679fcd727f34cafc6f3607eebcd7
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-09-11 11:22:07 +0300

    updated init

--- models/base_model.py
+++ models/base_model.py
@@ -10,13 +10,20 @@ class BaseModel:
     """
     Defines all common attributes/methods for other classes.
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initializes instance attributes.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if len(kwargs) == 0:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        value = datetime.fromisoformat(value)
+                    setattr(self, key, value)
 
     def __str__(self):
         """
