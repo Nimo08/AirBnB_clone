5168dc0ea76870aa7b0477977872ffa7823db939
Nimo08 <nimomohamed800@gmail.com>
2023-09-11 09:33:09 +0300

    datetime

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 Module contains BaseModel class.
 """
 import uuid
-import datetime
+from datetime import datetime
 
 
 class BaseModel:
@@ -15,8 +15,8 @@ class BaseModel:
         Initializes instance attributes.
         """
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """
@@ -30,7 +30,7 @@ class BaseModel:
         Updates the public instance attribute updated_at
         with the current datetime.
         """
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """
