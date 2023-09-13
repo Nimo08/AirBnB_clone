5d3bc655789f4575862fce705b1f596f5f2d297b
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-09-13 15:57:48 +0300

    storage tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -5,6 +5,11 @@ file storage tests
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.amenity import Amenity
+from models.review import Review
+from models.city import City
 import models
 import unittest
 import json
@@ -48,15 +53,55 @@ class TestFileStorage(unittest.TestCase):
         key = f"{obj.__class__.__name__}.{obj.id}"
         self.assertEqual(all_objs[key], obj)
     
+    def test_new_Amenity(self):
+        """testing new"""
+        obj = Amenity()
+        all_objs = models.storage.all()
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.assertEqual(all_objs[key], obj)
+    
+    def test_new_City(self):
+        """testing new"""
+        obj = City()
+        all_objs = models.storage.all()
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.assertEqual(all_objs[key], obj)
+    
+    def test_new_Place(self):
+        """testing new"""
+        obj = Place()
+        all_objs = models.storage.all()
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.assertEqual(all_objs[key], obj)
+    
+    def test_new_Review(self):
+        """testing new"""
+        obj = Review()
+        all_objs = models.storage.all()
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.assertEqual(all_objs[key], obj)
+    
+    def test_new_State(self):
+        """testing new"""
+        obj = State()
+        all_objs = models.storage.all()
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.assertEqual(all_objs[key], obj)
+    
     def test_new_all(self):
         """testing new"""
         obj = User()
         obj2 = BaseModel()
+        obj3 = Amenity()
+        obj4 = City()
+        obj5 = Place()
+        obj6 = Review()
+        obj7 = State()
+        list_objs = [obj, obj2, obj3, obj4, obj5, obj6, obj7]
         all_objs = models.storage.all()
-        key = f"{obj.__class__.__name__}.{obj.id}"
-        self.assertEqual(all_objs[key], obj)
-        key = f"{obj2.__class__.__name__}.{obj2.id}"
-        self.assertEqual(all_objs[key], obj2)
+        for ob in list_objs:
+            key = f"{ob.__class__.__name__}.{ob.id}"
+            self.assertEqual(all_objs[key], ob)
 
     def test_save_BaseModel(self):
         """ testing save"""
@@ -113,6 +158,56 @@ class TestFileStorage(unittest.TestCase):
             json_file = json.load(f)
         self.assertEqual(json_file, expected)
         self.assertEqual(len(models.storage._FileStorage__objects), 3)
+
+    def test_save_Amenity(self):
+        """ testing save"""
+        obj1 = Amenity()
+        models.storage.save()
+        expected = {f"{obj1.__class__.__name__}.{obj1.id}": obj1.to_dict()}
+        json_filename = "file.json"
+        with open(json_filename, 'r') as f:
+            json_file = json.load(f)
+        self.assertEqual(json_file, expected)
+
+    def test_save_City(self):
+        """ testing save"""
+        obj1 = City()
+        models.storage.save()
+        expected = {f"{obj1.__class__.__name__}.{obj1.id}": obj1.to_dict()}
+        json_filename = "file.json"
+        with open(json_filename, 'r') as f:
+            json_file = json.load(f)
+        self.assertEqual(json_file, expected)
+    
+    def test_save_Place(self):
+        """ testing save"""
+        obj1 = Place()
+        models.storage.save()
+        expected = {f"{obj1.__class__.__name__}.{obj1.id}": obj1.to_dict()}
+        json_filename = "file.json"
+        with open(json_filename, 'r') as f:
+            json_file = json.load(f)
+        self.assertEqual(json_file, expected)
+    
+    def test_save_Review(self):
+        """ testing save"""
+        obj1 = Review()
+        models.storage.save()
+        expected = {f"{obj1.__class__.__name__}.{obj1.id}": obj1.to_dict()}
+        json_filename = "file.json"
+        with open(json_filename, 'r') as f:
+            json_file = json.load(f)
+        self.assertEqual(json_file, expected)
+    
+    def test_save_State(self):
+        """ testing save"""
+        obj1 = State()
+        models.storage.save()
+        expected = {f"{obj1.__class__.__name__}.{obj1.id}": obj1.to_dict()}
+        json_filename = "file.json"
+        with open(json_filename, 'r') as f:
+            json_file = json.load(f)
+        self.assertEqual(json_file, expected)
     
     def test_save_all(self):
         """ testing save"""
@@ -131,6 +226,22 @@ class TestFileStorage(unittest.TestCase):
         with open(json_filename, 'r') as f:
             json_file = json.load(f)
         self.assertEqual(json_file, expected)
+        obj4 = City()
+        obj5 = Place()
+        obj6 = Review()
+        obj7 = State()
+        models.storage.save()
+        list_objs = [obj4, obj5, obj6, obj7]
+        for ob in list_objs:
+            key = f"{ob.__class__.__name__}.{ob.id}"
+            expected[key] = ob.to_dict()
+        with open(json_filename, 'r') as f:
+            json_file = json.load(f)
+        self.assertEqual(json_file, expected)
+    
+    def test_reload_empty(self):
+        """reload no json file"""
+        models.storage.reload()
 
     def test_reload_BaseModel(self):
         """testing reload"""
@@ -168,23 +279,109 @@ class TestFileStorage(unittest.TestCase):
             value1_dict.append(value.to_dict())
         self.assertEqual(value_dict, value1_dict)
     
-    def test_reload_User(self):
+    def test_reload_Amenity(self):
         """testing reload"""
-        user = User()
-        base = BaseModel()
-        key_user = f"{user.__class__.__name__}.{user.id}"
-        key_base = f"{base.__class__.__name__}.{base.id}"
+        amenity = Amenity()
+        key_amenity = f"{amenity.__class__.__name__}.{amenity.id}"
         obj = models.storage.all()
         models.storage.save()
         models.storage.reload()
         obj1 = models.storage.all()
         json_filename = "file.json"
         value_dict = []
-        value_dict.append(obj[key_user].to_dict())
-        value_dict.append(obj[key_base].to_dict())
+        value_dict.append(obj[key_amenity].to_dict())
         value1_dict = []
-        value1_dict.append(obj1[key_user].to_dict())
-        value1_dict.append(obj1[key_base].to_dict())
+        value1_dict.append(obj1[key_amenity].to_dict())
+        self.assertEqual(value_dict, value1_dict)
+    
+    def test_reload_City(self):
+        """testing reload"""
+        city = City()
+        key_city = f"{city.__class__.__name__}.{city.id}"
+        obj = models.storage.all()
+        models.storage.save()
+        models.storage.reload()
+        obj1 = models.storage.all()
+        json_filename = "file.json"
+        value_dict = []
+        value_dict.append(obj[key_city].to_dict())
+        value1_dict = []
+        value1_dict.append(obj1[key_city].to_dict())
+        self.assertEqual(value_dict, value1_dict)
+    
+    def test_reload_Place(self):
+        """testing reload"""
+        place = Place()
+        key_place = f"{place.__class__.__name__}.{place.id}"
+        obj = models.storage.all()
+        models.storage.save()
+        models.storage.reload()
+        obj1 = models.storage.all()
+        json_filename = "file.json"
+        value_dict = []
+        value_dict.append(obj[key_place].to_dict())
+        value1_dict = []
+        value1_dict.append(obj1[key_place].to_dict())
+        self.assertEqual(value_dict, value1_dict)
+    
+    def test_reload_Review(self):
+        """testing reload"""
+        review = Review()
+        key_review = f"{review.__class__.__name__}.{review.id}"
+        obj = models.storage.all()
+        models.storage.save()
+        models.storage.reload()
+        obj1 = models.storage.all()
+        json_filename = "file.json"
+        value_dict = []
+        value_dict.append(obj[key_review].to_dict())
+        value1_dict = []
+        value1_dict.append(obj1[key_review].to_dict())
+        self.assertEqual(value_dict, value1_dict)
+    
+    def test_reload_State(self):
+        """testing reload"""
+        state = State()
+        key_state = f"{state.__class__.__name__}.{state.id}"
+        obj = models.storage.all()
+        models.storage.save()
+        models.storage.reload()
+        obj1 = models.storage.all()
+        json_filename = "file.json"
+        value_dict = []
+        value_dict.append(obj[key_state].to_dict())
+        value1_dict = []
+        value1_dict.append(obj1[key_state].to_dict())
+        self.assertEqual(value_dict, value1_dict)
+    
+    def test_reload_All(self):
+        """testing reload"""
+        user = User()
+        base = BaseModel()
+        obj3 = Amenity()
+        obj4 = City()
+        obj5 = Place()
+        obj6 = Review()
+        obj7 = State()
+        key_base = f"{base.__class__.__name__}.{base.id}"
+        key_user = f"{user.__class__.__name__}.{user.id}"
+        key_amenity = f"{obj3.__class__.__name__}.{obj3.id}"
+        key_city = f"{obj4.__class__.__name__}.{obj4.id}"
+        key_place = f"{obj5.__class__.__name__}.{obj5.id}"
+        key_review = f"{obj6.__class__.__name__}.{obj6.id}"
+        key_state = f"{obj7.__class__.__name__}.{obj7.id}"
+        obj = models.storage.all()
+        models.storage.save()
+        models.storage.reload()
+        obj1 = models.storage.all()
+        json_filename = "file.json"
+        value_dict = []
+        key_list = [key_place, key_amenity, key_city, key_review, key_user, key_base, key_state]
+        for key in key_list:
+            value_dict.append(obj[key].to_dict())
+        value1_dict = []
+        for key in key_list:
+            value1_dict.append(obj1[key].to_dict())
         self.assertEqual(value_dict, value1_dict)
 
     def tearDown(self):
